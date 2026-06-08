2d1f870316523853430d8a4cd527c5a3ea34dd1b
Whiskeysmoke <vikranth.choudary006@gmail.com>
2026-06-08 23:25:37 +0530

    feat: add web interface

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
+import gradio as gr
 
 load_dotenv()
 
@@ -15,11 +16,20 @@ llm = ChatOllama(model="qwen2.5:3b")
 
 system_prompt = """
 You are a helpful assistant.
+Answer all user's queries.
 Use the get_date tool if the user is asking about today's date.
 """
 
 agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
-user_query = input("Enter a query: ")
-response = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
 
-print(response['messages'][-1].content)
+def chat(message, history):
+    response = agent.invoke(
+        {"messages": [{"role": "user", "content": message}]})
+    last_response = response['messages'][-1].content
+    return last_response
+
+with gr.Blocks() as demo:
+    gr.Markdown("# AI Chatbot")
+    gr.ChatInterface(fn=chat)
+
+demo.launch()
