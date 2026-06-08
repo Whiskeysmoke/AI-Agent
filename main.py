19f647dffd111e61b6ab1925b4508e7e142c377e
Whiskeysmoke <vikranth.choudary006@gmail.com>
2026-06-08 23:51:03 +0530

    feat: add web searching capabilities to agent

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 from dotenv import load_dotenv
 from datetime import datetime
@@ -9,12 +10,15 @@ import gradio as gr
 from langgraph.checkpoint.sqlite import SqliteSaver
 import sqlite3
 
+from langchain_community.tools.tavily_search import TavilySearchResults
+
 load_dotenv()
 
 def get_date():
     """Get the current date"""
     return datetime.now().strftime("%Y-%m-%d")
 
+search_tool = TavilySearchResults()
 conn = sqlite3.connect("chatbot_memory.db", check_same_thread=False)
 checkpointer = SqliteSaver(conn)
 
@@ -23,11 +27,12 @@ llm = ChatOllama(model="qwen2.5:3b")
 system_prompt = """
 You are a helpful assistant.
 Answer all user's queries.
-Use the get_date tool if the user is asking about today's date.
+Use the get_date tool ONLY if the user is explicitly asking about today's date.
+Use the search_tool for answering questions that require up to date information.
 """
 
 agent = create_agent(model=llm,
-                     tools=[get_date],
+                     tools=[get_date, search_tool],
                      system_prompt=system_prompt,
                      checkpointer=checkpointer)
 
