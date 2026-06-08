18f63a9378ad5e47dd13ee13378eeb199b7cc330
Whiskeysmoke <vikranth.choudary006@gmail.com>
2026-06-08 23:15:03 +0530

    refactor: changed the model from a cloud Gemini API key to local Ollama

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime
 
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
 
 load_dotenv()
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 def get_date():
     """Get the current date"""
     return datetime.now().strftime("%Y-%m-%d")
 
-llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview")
+llm = ChatOllama(model="qwen2.5:3b")
 
 system_prompt = """
 You are a helpful assistant.
@@ -23,4 +22,4 @@ agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
 user_query = input("Enter a query: ")
 response = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
 
-print(response['messages'][-1].content[0]['text'])
+print(response['messages'][-1].content)
